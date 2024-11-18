ee2311a421c921dc534a50cbccfeb8cbcd168225
fatima-hasan8 <156367497+fatima-hasan8@users.noreply.github.com>
2024-11-18 10:52:09 +0500

    jjjj

---
+++ ship.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // include library for input/output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // output Hello, World!
+    std::cout << "Hello!" << std::endl;
+    return 0; // return 0 to indicate successful execution
+}
