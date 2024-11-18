e9376bde1be2ebf8430698bc8b9d9d86b6117030
fatima-hasan8 <156367497+fatima-hasan8@users.noreply.github.com>
2024-11-18 10:46:30 +0500

    helllooo

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // include library for input/output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // output Hello, World!
+    return 0; // return 0 to indicate successful execution
+}
+
